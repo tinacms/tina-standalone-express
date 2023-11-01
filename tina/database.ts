51f2c65216d0b405131bb31f450d30ae14856845
Logan Anderson <landerson2@upei.ca>
2023-11-01 14:43:13 -0400

    improve branch

--- tina/database.ts
+++ tina/database.ts
@@ -9,17 +9,23 @@ const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
 
 console.log("isLocal", isLocal);
 
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
 export default isLocal
   ? createLocalDatabase()
   : createDatabase({
       gitProvider: new GitHubProvider({
-        branch: process.env.GITHUB_BRANCH!,
+        branch,
         owner: process.env.GITHUB_OWNER!,
         repo: process.env.GITHUB_REPO!,
         token: process.env.GITHUB_PERSONAL_ACCESS_TOKEN!,
       }),
       databaseAdapter: new MongodbLevel<string, Record<string, unknown>>({
-        collectionName: process.env.GITHUB_BRANCH!,
+        collectionName,
         dbName: "tinacms-test-oct-31",
         mongoUri: process.env.MONGODB_URI!,
       }),
